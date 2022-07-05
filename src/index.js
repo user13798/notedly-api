347439b0057d1fcf511a9f339a5af5ff2305f979
user13798 <71964046+user13798@users.noreply.github.com>
2022-07-05 16:02:51 +0300

    add comments

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const models = require("./models");
 const typeDefs = require("./schema.js");
 const resolvers = require("./resolvers");
 
+// Run our server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
 const DB_HOST = process.env.DB_HOST;
 
@@ -15,27 +16,36 @@ const app = express();
 
 db.connect(DB_HOST);
 
+// get the user info from a JWT
 const getUser = (token) => {
   if (token) {
     try {
+      // return the user information from the token
       return jwt.verify(token, process.env.JWT_SECRET);
     } catch (err) {
+      // if there's a problem with the token, throw an error
       throw new Error("Session invalid");
     }
   }
 };
 
+// Apollo Server setup
+// updated to include `validationRules`
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => {
+    // get the user token from the headers
     const token = req.headers.authorization;
+    // try to retrieve a user with the token
     const user = getUser(token);
     console.log(user);
+    // add the db models and the user to the context
     return { models, user };
   },
 });
 
+// Apply the Apollo GraphQL middleware and set the path to /api
 server.applyMiddleware({ app, path: "/api" });
 
 app.listen(port, () => {
