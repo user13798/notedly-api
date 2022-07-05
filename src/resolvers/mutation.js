aac5ed6b1d93323805363538f0531f1310f50874
user13798 <71964046+user13798@users.noreply.github.com>
2022-07-05 16:21:03 +0300

    add comments

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -71,9 +71,15 @@ module.exports = {
     }
   },
   signUp: async (parent, { username, email, password }, { models }) => {
+    // normalize email address
     email = email.trim().toLowerCase();
+
+    // hash the password
     const hashed = await bcrypt.hash(password, 10);
+
+    // create the gravatar url
     const avatar = gravatar(email);
+
     try {
       const user = await models.User.create({
         username,
@@ -82,27 +88,36 @@ module.exports = {
         avatar,
       });
 
+      // create and return the json web token
       return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     } catch (error) {
       console.log(error);
+      // if there's a problem creating the account, throw an error
       throw new Error("Error creating account");
     }
   },
   signIn: async (parent, { username, email, password }, { models }) => {
     if (email) {
+      // normalize email address
       email = email.trim().toLowerCase();
     }
+
     const user = await models.User.findOne({
       $or: [{ username }, { email }],
     });
+
+    // if no user is found, throw an authentication error
     if (!user) {
       throw new AuthenticationError("Error signing in");
     }
+
+    // if the passwords don't match, throw an authentication error
     const valid = await bcrypt.compare(password, user.password);
     if (!valid) {
       throw new AuthenticationError("Error signing in");
     }
 
+    // create and return json web token
     return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
   },
 };
